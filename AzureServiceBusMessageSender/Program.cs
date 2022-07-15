c04fc7e2a5b9fe83ead5611d8b421df23eeeb8f0
diki_ <diki_@DESKTOP-M5N51NC>
2022-07-15 23:41:26 +0200

    Bulk process messages

--- AzureServiceBusMessageSender/Program.cs
+++ AzureServiceBusMessageSender/Program.cs
@@ -14,7 +14,7 @@ ServiceBusClient client;
 ServiceBusSender sender;
 
 // number of messages to be sent to the topic
-const int numOfMessages = 3;
+const int numOfMessages = 5;
 
 
 // The Service Bus client types are safe to cache and use as a singleton for the lifetime
--- BulkMessaging/Function1.cs
+++ BulkMessaging/Function1.cs
@@ -1,6 +1,5 @@
 using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace BulkMessaging
@@ -8,16 +7,20 @@ namespace BulkMessaging
     public class Function1
     {
         private readonly ILogger<Function1> _logger;
-
+        
         public Function1(ILogger<Function1> log)
         {
             _logger = log;
         }
 
         [FunctionName("ProcessBulkMessages")]
-        public void Run([ServiceBusTrigger("bulk-message", "ConsoleSubscription", Connection = "connectionstring")]string mySbMsg)
+        public void Run([ServiceBusTrigger("bulk-message", "ConsoleSubscription", Connection = "connectionstring")]string[] mySbMsg)
         {
-            _logger.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+            foreach (var item in mySbMsg)
+            {
+                Console.WriteLine(item);
+                _logger.LogInformation($"C# ServiceBus topic trigger function processed message: {item}");
+            }
         }
     }
 }
